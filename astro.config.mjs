2b430229819abd54c5a97701766d7e200b245262
Vivekworks114 <vivekworks114@gmail.com>
2026-07-30 22:38:39 +0530

    changes done

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 // https://astro.build/config
 export default defineConfig({
